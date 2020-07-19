7a2aa119c774a05b926154633a8b8c05fabe1edd
Divyansh-saxena <64483506+Divyansh-saxena@users.noreply.github.com>
2020-07-19 14:16:33 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+ <center> WECOME TO MY CLOUD WORLD </center> 
+<br />
+<pre>
+<?php
+echo' 
+<html>
+</html>
+';
+echo'
+<center> <h2> Great initiative by LinuxWorld Informatics Pvt. Ltd. Making India Future Ready </h2> </center>
+';
+?>
+</pre>
